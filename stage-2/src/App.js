c0c2042475b6c8fce0a39c211d6f2f8aae027af8
ZachGerhart <zgerhart@gmail.com>
2017-12-13 21:57:16 -0600

    fixed all the bugs

--- stage-2/src/App.js
+++ stage-2/src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home/Home';
 import ShoppingCart from './components/ShoppingCart/ShoppingCart';
 import StoreFront from './components/StoreFront/StoreFront';
 import { Route, Switch } from 'react-router-dom';
+import { Link }  from 'react-router-dom'
 
 
 class App extends Component {
